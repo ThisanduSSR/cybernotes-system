8f24788386b98bfec8b119c57f98436bb01c9ace
ThisanduSSR <thisanduranadheera67@gmail.com>
2026-05-24 14:50:13 +0530

    Create user entity and repository structure

--- backend/backend/backend/src/main/java/repository/UserRepository.java
+++ backend/backend/backend/src/main/java/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package backend.repository;
+package repository;
 
-import backend.model.User;
+import model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
