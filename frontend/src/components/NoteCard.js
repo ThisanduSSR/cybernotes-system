bc0b4934c4db6f3c811496611517cb540bcf1e07
ThisanduSSR <thisanduranadheera67@gmail.com>
2026-05-25 19:54:54 +0530

    comment: document Modal glassmorphism configuration rules

--- frontend/src/components/NoteCard.js
+++ frontend/src/components/NoteCard.js
@@ -88,3 +88,5 @@ function NoteCard({ note, onDelete, onUpdate }) {
 export default NoteCard;
 
 // Developer Note: Glass card borders use semi-transparent white boundaries to mimic glass effect.
+
+// Developer Note: Modals use contentClassName to inherit glassmorphic styles directly.
